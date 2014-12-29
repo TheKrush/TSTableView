18a5168813245fd9556ac93505637ccb1133f643
Noam Gat <noamgat@gmail.com>
2014-12-29 16:32:30 +0200

    Demo improvements

--- Examples/SimpleTableViewController.cs
+++ Examples/SimpleTableViewController.cs
@@ -4,9 +4,10 @@ using Tacticsoft;
 
 namespace Tacticsoft.Examples
 {
+    //An example implementation of a class that communicates with a TableView
     public class SimpleTableViewController : MonoBehaviour, ITableViewDataSource, ITableViewDelegate
     {
-        public VisibleCounterCell m_labelPrefab;
+        public VisibleCounterCell m_cellPrefab;
         public TableView m_tableView;
 
         public int m_numRows;
@@ -18,6 +19,10 @@ namespace Tacticsoft.Examples
             m_tableView.tableViewDataSource = this;
         }
 
+        public void SendBeer() {
+            Application.OpenURL("https://www.paypal.com/cgi-bin/webscr?business=contact@tacticsoft.net&cmd=_xclick&item_name=Beer%20for%20TSTableView&currency_code=USD&amount=5.00");
+        }
+
         #region ITableViewDataSource
 
         //Will be called by the TableView to know how many rows are in this table
@@ -27,14 +32,14 @@ namespace Tacticsoft.Examples
 
         //Will be called by the TableView to know what is the height of each row
         public float GetHeightForRowInTableView(TableView tableView, int row) {
-            return (m_labelPrefab.transform as RectTransform).rect.height;
+            return (m_cellPrefab.transform as RectTransform).rect.height;
         }
 
         //Will be called by the TableView when a cell needs to be created for display
         public TableViewCell GetCellForRowInTableView(TableView tableView, int row) {
-            VisibleCounterCell cell = tableView.GetReusableCell(m_labelPrefab.reuseIdentifier) as VisibleCounterCell;
+            VisibleCounterCell cell = tableView.GetReusableCell(m_cellPrefab.reuseIdentifier) as VisibleCounterCell;
             if (cell == null) {
-                cell = (VisibleCounterCell)GameObject.Instantiate(m_labelPrefab);
+                cell = (VisibleCounterCell)GameObject.Instantiate(m_cellPrefab);
             }
             cell.SetRowNumber(row);
             cell.gameObject.name = "Row_" + row.ToString();
