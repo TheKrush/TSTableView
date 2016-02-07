f842967b4f1677d0e199d80f6ddf31f2cefde571
Ariel Bluman <ariel@tacticsoft.net>
2016-02-07 13:47:20 +0200

    Color for cells

--- Examples/Simple/VisibleCounterCell.cs
+++ Examples/Simple/VisibleCounterCell.cs
@@ -11,15 +11,28 @@ namespace Tacticsoft.Examples
     {
         public Text m_rowNumberText;
         public Text m_visibleCountText;
+        public Image m_background;
 
         public void SetRowNumber(int rowNumber) {
             m_rowNumberText.text = "Row " + rowNumber.ToString();
+            m_background.color = GetColorForRow(rowNumber);
         }
 
         private int m_numTimesBecameVisible;
         public void NotifyBecameVisible() {
             m_numTimesBecameVisible++;
             m_visibleCountText.text = "# rows this cell showed : " + m_numTimesBecameVisible.ToString();
+        }
+
+        private Color GetColorForRow(int row) {
+            switch (row % 3) {
+                case 0:
+                    return Color.gray;
+                case 1:
+                    return Color.white;
+                default:
+                    return Color.red;
+            }
         }
 
     }
